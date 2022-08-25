9382e690250a14a4f14d9126f92020e7acd09a5c
Kristina Faye <kglinoga@gmail.com>
2022-08-25 15:13:07 -0700

    no styling, but header, Main/Home, and footer all rendering

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,6 +4,7 @@ export default function Header() {
     return (
         <>
             <h1 className="">Kristina Glinoga</h1>
+            
         </>
     );
 };
--- src/components/PortContainer.js
+++ src/components/PortContainer.js
@@ -1,46 +1,22 @@
 import React, { useState } from 'react';
 import Header from './Header';
 import Footer from './Footer';
-import NavTabs from './NavTabs';
+// import NavTabs from './NavTabs';
 import Home from './pages/Home';
 // import About from './pages/About';
 // import Contact from './pages/Contact';
 
 
 export default function PortContainer() {
-    const [currentPage, setCurrentPage] = useState('Home');
+
  
-    const renderPage = () => {
-        if (currentPage === 'Home') {
-            return <Home />;
-        }
-        return;
-    };
-
-    const handlePageChange = (page) => setCurrentPage(page);
-
     return (
         <>
             <Header />
-            <NavTabs currentPage={currentPage}
-                handlePageChange={handlePageChange} />
-            {renderPage()}
+            <Home />
             <Footer />
 
         </>
     );
 
-    // const [currentPage, setCurrentPage] = useState('Home');
-    // const renderPage = () => {
-    //     if (currentPage === 'Home') {
-    //         return <Home />;
-    //     }
-    //     if (currentPage === 'About') {
-    //         return <About />;
-    //     }
-    //     if (currentPage === 'Project') {
-    //         return <Project />;
-    //     }
-    //     return 
-    // };
 }
