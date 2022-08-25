942eb2cf11cf0caeb3b2604d383b42b7b99732d6
Kristina Faye <kglinoga@gmail.com>
2022-08-25 16:31:11 -0700

    changed title

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -4,7 +4,7 @@ import React from 'react';
 // We assign them to their own variable names
 function Navigation({ currentPage, handlePageChange }) {
     return (
-        <ul className="nav nav-tabs">
+        <ul className="nav ">
             <li className="nav-item">
                 <a
                     href="#about"
