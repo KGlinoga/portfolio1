36306389385a21c529618fe8f4de138a96104e4b
Kristina Faye <kglinoga@gmail.com>
2022-08-25 16:20:50 -0700

    attempted an array of objects for Project.. will come back after formatting is done

--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -1,12 +1,22 @@
 import React from 'react';
 // import logo from './logo192.png';
 
+// lines 5-13 are my attempt to set up an array for a whole list of projects.  Will keep hardcoded for now.  Will do formatting instead, circle back to this.
+// const projects = [
+//     {
+//         image: "https://secure.img1-fg.wfcdn.com/im/07042854/resize-h445%5Ecompr-r85/4518/45180826/Wrapped+Canvas+Photograph.jpg",
+//         title: "Application One",
+//         description: "Describing Application One! Selling it really really well so y'all wanna try it out!",
+//         deployedLink: "www.youtube.com",
+//         gitLink: "https://github.com/KGlinoga"
+//     },
+// ];
+
 export default function Project() {
     return (
         <div>
             {/* image, Title, description, link to deployed app, link to GitHub repo */}
             {/* need to find syntax for images, and links. */}
-            {/* {/* <image> </image> */}
             <img src={"https://secure.img1-fg.wfcdn.com/im/07042854/resize-h445%5Ecompr-r85/4518/45180826/Wrapped+Canvas+Photograph.jpg"} alt=" beautiful sunset" />
             <h3>App Title</h3>
             <p>description</p>
