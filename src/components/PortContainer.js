f1a1e6334445f675b8752f03eec53584bec41b76
Kristina Faye <kglinoga@gmail.com>
2022-08-25 16:08:53 -0700

    images from the internet work! probably going to utilize google docs hosting for links b/c relative paths weren't working for me

--- src/components/PortContainer.js
+++ src/components/PortContainer.js
@@ -1,20 +1,36 @@
 import React, { useState } from 'react';
 import Header from './Header';
+import Navigation from './Navigation';
 import About from './pages/About';
 import Portfolio from './pages/Portfolio';
 import Footer from './Footer';
 import Contact from './pages/Contact';
 
-// import NavTabs from './NavTabs';
-
-
 export default function PortContainer() {
+    const [currentPage, setCurrentPage] = useState('About');
+
+    const renderPage = () => {
+        if (currentPage === 'About') {
+            return <About />;
+        }
+        if (currentPage === 'Portfolio') {
+            return <Portfolio />;
+        }
+        return <Contact />;
+    };
+
+    const handlePageChange = (page) => setCurrentPage(page);
+    
     return (
         <>
             <Header />
-            <About />
+            <Navigation
+                currentPage={currentPage}
+                handlePageChange={handlePageChange} />
+            {renderPage()}
+            {/* <About />
             <Portfolio />
-            <Contact />
+            <Contact /> */}
             <Footer />
 
         </>
--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -1,4 +1,5 @@
 import React from 'react';
+// import logo from './logo192.png';
 
 export default function Project() {
     return (
@@ -6,10 +7,12 @@ export default function Project() {
             {/* image, Title, description, link to deployed app, link to GitHub repo */}
             {/* need to find syntax for images, and links. */}
             {/* {/* <image> </image> */}
+            <img src={"https://secure.img1-fg.wfcdn.com/im/07042854/resize-h445%5Ecompr-r85/4518/45180826/Wrapped+Canvas+Photograph.jpg"} alt=" beautiful sunset" />
             <h3>App Title</h3>
             <p>description</p>
             {/* <link>Deployed App</link>
             <link>GitHub Repo</link> */}
+            <a href="https://github.com/KGlinoga">github</a>
 
         </div>
     )
