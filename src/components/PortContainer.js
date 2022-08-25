6975285ee1227cd0caeec572f6dfa0a829c0f8a1
Kristina Faye <kglinoga@gmail.com>
2022-08-25 15:17:01 -0700

    Home changed to About, and updated copy

--- src/components/PortContainer.js
+++ src/components/PortContainer.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import Header from './Header';
 import Footer from './Footer';
 // import NavTabs from './NavTabs';
-import Home from './pages/Home';
+import Home from './pages/About';
 // import About from './pages/About';
 // import Contact from './pages/Contact';
 
---
+++ src/components/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function About() {
+    return (
+        <div>
+            <h1>Welcome to my Portfolio!</h1>
+            <p>
+                My name is Kristina Glinoga.  I am a budding web developer, looking for folks who value equity, work/life balance, and social justice. 
+            </p>
+            <p>In the following sections you can find my projects and contact info.</p>
+            <p>Please reach out via email, GitHub or LinkedIn if you want to get in touch!</p>
+        </div>
+    );
+}
