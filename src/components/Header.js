4022c78a61acc588d96a0f323a57cb29b4a60a2b
Kristina Faye <kglinoga@gmail.com>
2022-09-22 10:30:48 -0700

    updated Contact page to include Upwork/LinkedIn and commented out the form. it's impressive, but I need this to run just on GH-pages

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,12 @@ export default function Header() {
     return (
         <div className="header row">
             <div className="header col-md-12">
-                <h1 className="header">Kristina Glinoga</h1> <h4>Web Developer</h4> 
+                <h1 className="header">Kristina Glinoga</h1>
+                <h6>Software Engineer</h6> 
+                <h6>Website/App Developer</h6> 
+                <h6>Content Creator</h6> 
+                <h6>Product Manager</h6> 
+                
             {/* <Navigation /> */}
             </div>
         </div>
--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Form from '../Form/index';
+// import Form from '../Form/index';
 
 export default function Contact() {
     return (
@@ -9,12 +9,21 @@ export default function Contact() {
                 Thank you for your time and consideration!
             </p>
             <p>Please reach out via the following channels: </p>
+
             <p>email: <a href="mailto:kglinoga@gmail.com" target="_blank" rel="noopener noreferrer">kglinoga@gmail.com</a></p>
+
+            <p>UpWork: <a href="https://www.upwork.com/freelancers/~0126bc4133e3045ddd" target="_blank" rel="noopener noreferrer">Kristina on Upwork!</a></p>
+
+            <p>LinkedIn: <a href="https://www.linkedin.com/in/kristina-glinoga/" target="_blank" rel="noopener noreferrer">Kristina on LinkedIn!</a></p>
+
             <p>Instagram: @kristinacodes (Coming Soon!)</p>
+
             <p>YouTube: <a href="https://www.youtube.com/butchery101" target="_blank" rel="noopener noreferrer">@butchery101</a></p>
+
             <p>GitHub: <a href="https://github.com/KGlinoga" target="_blank" rel="noopener noreferrer">Kristina Glinoga</a></p>
+
             <a href="./assets/Resume.pdf" target="_blank" rel="noopener noreferrer">Resume</a>
-            <Form />
+            {/* <Form /> */}
 
         </div>
     );
